3b4d3cf1c7e6054ee5cf06cac1972c87c2aeddf6
cdb <13335137159@163.com>
2021-03-03 19:40:14 +0800

    admin 修改

--- rainingsince-framework-core/spring-admin/src/main/java/com/rainingsince/admin/plugs/MyMetaObjectHandler.java
+++ rainingsince-framework-core/spring-admin/src/main/java/com/rainingsince/admin/plugs/MyMetaObjectHandler.java
@@ -1,9 +1,11 @@
 package com.rainingsince.admin.plugs;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.rainingsince.web.context.ApplicationProvider;
 import com.rainingsince.web.context.RequestContext;
 import org.apache.ibatis.reflection.MetaObject;
+
 import java.util.Date;
 
 public class MyMetaObjectHandler implements MetaObjectHandler {
@@ -13,10 +15,13 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
         Date date = new Date();
         RequestContext requestContext = ApplicationProvider.
                 getRequestContext(RequestContext.class);
-        this.setInsertFieldValByName("createBy",
-                requestContext.getUserId(), metaObject);
-        this.setInsertFieldValByName("updateBy",
-                requestContext.getUserId(), metaObject);
+        if (StringUtils.isNotEmpty(requestContext.getUserId())) {
+            if (StringUtils.isEmpty((String) this.getFieldValByName("createBy", metaObject)))
+                this.setInsertFieldValByName("createBy",
+                        requestContext.getUserId(), metaObject);
+            this.setInsertFieldValByName("updateBy",
+                    requestContext.getUserId(), metaObject);
+        }
         this.setInsertFieldValByName("createDate",
                 date, metaObject);
         this.setInsertFieldValByName("updateDate",
